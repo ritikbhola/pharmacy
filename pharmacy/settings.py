435cf4234c48031f6ec9289359502e719738655c
ritik <ritikbhola321456@gmail.com>
2022-03-20 03:22:10 +0530

    commit

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pr36_2ll)7*8mrld7r#a73xx68uzx%*r6g*u1ra@xggwowlrzj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
