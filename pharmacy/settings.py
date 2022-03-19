d5c04b92efa0609c940313077f0c1d68be592f29
ritik <ritikbhola321456@gmail.com>
2022-03-20 03:25:39 +0530

    commit

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pr36_2ll)7*8mrld7r#a73xx68uzx%*r6g*u1ra@xggwowlrzj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','pharmacyo.herokuapp.com']
 
 
 # Application definition
